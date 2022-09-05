c9bac04e51acd3635e7a63c62f840f7bb472bb5a
Ramazan Amanbayev <Ramazan.Amanbayev@isate.kz>
2022-09-05 21:21:39 +0600

    Add project files.

---
+++ LinkedList/Program.cs
@@ -0,0 +1,13 @@
+﻿using LinkedList;
+
+var list = new DoublyLinkedList<int>();
+list.AddLast(3);   
+list.AddLast(1);   
+list.AddLast(2);
+
+list.Sort();
+
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+}
